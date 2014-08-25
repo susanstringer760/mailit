458927c4b4345584f69a3989d0671270dedccf98
Susan Stringer <snorman@ucar.edu>
2014-08-25 13:11:55 -0600

    initial commit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ActionMailer::Base
+
+  default :from => "susanstringer760@gmail.com"
+  
+  def registration_confirmation(user)
+    @user = user
+    mail(:to => "#{@user.name} <#{@user.email}>", :subject => "Registered")
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailit::Application.config.secret_token = 'd718956cae79d059c1dad8864c398641f0ae73a66a76d2ea698f025f1491e87f6219c0365e15b492b1f5ffd84ab9c308aff9c2237042658d288cab1fe7c90404'
